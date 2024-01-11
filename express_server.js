f2c579770ee7cd45f9cf8bcbdd24ee9a7fe27456
Chris De Asis <138704176+cde-as@users.noreply.github.com>
2024-01-11 15:23:39 -0800

    Create URL Submission Form

--- express_server.js
+++ express_server.js
@@ -30,6 +30,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: req.params.longURL/* What goes here? */ };
   res.render("urls_show", templateVars);
