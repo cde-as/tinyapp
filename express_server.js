ef24eebfa00c9ee5a6bd46fa1b56d13bd9eb4b5d
Chris De Asis <138704176+cde-as@users.noreply.github.com>
2024-01-13 11:18:02 -0800

    Create Edit column and buttons

--- express_server.js
+++ express_server.js
@@ -37,9 +37,9 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-/* app.get("/hello", (req, res) => {
+app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
-}); */
+});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
