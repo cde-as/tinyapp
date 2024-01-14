7d36f861bb0007d1e9fb0c8050d449852adb867d
Chris De Asis <138704176+cde-as@users.noreply.github.com>
2024-01-13 16:08:24 -0800

    Cookies in Express, set login form and cookies

--- express_server.js
+++ express_server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
+
 const app = express();
 const PORT = 8080; // default port
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 //Creates our short URL string
 const generateRandomString = (length) => {
@@ -112,4 +115,20 @@ app.get("/urls/:id/edit", (req, res) => {
     longURL: urlDatabase[req.params.id],
   };
   res.render("urls_show", templateVars);
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  //const password = req.body.password;
+  console.log("Received username:", username);
+  //set the username cookie
+  res.cookie("username", username);
+  console.log("Cookie set:", req.cookies);
+
+  //redirect to /urls page
+  res.redirect("/urls");
 });
