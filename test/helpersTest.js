3ad97819480fdacedcb11ef05954da0e9cbe3997
Chris De Asis <138704176+cde-as@users.noreply.github.com>
2024-01-23 03:57:35 -0800

    add another it statement to test that a non-existent email returns undefined

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,26 +1,45 @@
-const { assert } = require('chai');
+const { assert } = require("chai");
 
-const { getUserByEmail } = require('../helpers.js');
+const { getUserByEmail } = require("../helpers.js");
 
 const testUsers = {
-  "userRandomID": {
+  userRandomID: {
     id: "userRandomID",
     email: "user@example.com",
-    password: "purple-monkey-dinosaur"
+    password: "purple-monkey-dinosaur",
   },
-  "user2RandomID": {
+  user2RandomID: {
     id: "user2RandomID",
     email: "user2@example.com",
-    password: "dishwasher-funk"
-  }
+    password: "dishwasher-funk",
+  },
 };
 
-describe('getUserByEmail', function() {
-  it('should return a user with valid email', function() {
+describe("getUserByEmail", function() {
+  it("should return a user with valid email", function() {
     const user = getUserByEmail("user@example.com", testUsers);
     const expectedUserID = "userRandomID";
+    const userEmail = "user@example.com";
 
-    // Write your assert statement here
-    assert.equal(user.id, expectedUserID), 'User ID should match the right user ID';
+    // Write your assert statements here
+    // Assert that the returned user's ID matches the expected user ID
+    assert.equal(user.id, expectedUserID,
+      "User ID should match the right user ID");
+
+    // Assert that the returned user's email matches the input email
+    assert.equal(
+      user.email,
+      userEmail,
+      "User email should match the input email"
+    );
   });
+
+  it('should return null for a non-existent email', function() {
+    const nonExistentEmail = "doesnotexist@example.com";
+    const user = getUserByEmail(nonExistentEmail, testUsers);
+
+    // Assert that the returned user is null for a non-existent email
+    assert.isNull(user, 'User should be null for a non-existent email');
+  });
+
 });
